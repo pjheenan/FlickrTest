90a96ede7bd3bc5dfedaf850dce264f4aa548918
Phil Heenan <pjheenan@gmail.com>
2017-06-09 17:54:22 +0100

    initial app setup
    the app module is responsible for:
    * android specific UI components
    * setting app any Dependency Injection
    * providing utility classes which require knowledge of the Android system

---
+++ app/src/androidTest/java/com/philheenan/discovrflickr/AppBaseAndroidTest.java
@@ -0,0 +1,7 @@
+package com.philheenan.discovrflickr;
+
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class) public class AppBaseAndroidTest {
+}
---
+++ app/src/test/java/com/philheenan/discovrflickr/AppBaseTest.java
@@ -0,0 +1,5 @@
+package com.philheenan.discovrflickr;
+
+public class AppBaseTest {
+
+}
