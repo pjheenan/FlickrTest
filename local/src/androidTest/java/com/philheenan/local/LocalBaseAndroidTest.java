91e026da19685f0ce1c7537dd290054e3388b42c
Phil Heenan <pjheenan@gmail.com>
2017-06-09 17:50:31 +0100

    create local module
    the local module provides handling for local storage functions. This storage can be in memory or persisted storage.

---
+++ local/src/androidTest/java/com/philheenan/local/LocalBaseAndroidTest.java
@@ -0,0 +1,7 @@
+package com.philheenan.local;
+
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class) public class LocalBaseAndroidTest {
+}
---
+++ local/src/test/java/com/philheenan/local/LocalBaseTest.java
@@ -0,0 +1,4 @@
+package com.philheenan.local;
+
+public class LocalBaseTest {
+}
