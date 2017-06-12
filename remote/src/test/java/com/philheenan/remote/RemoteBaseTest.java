4b5719eadfc5b25eaada9f723c8ba4de314946bd
Phil Heenan <pjheenan@gmail.com>
2017-06-12 15:12:30 +0100

    add gson instance for json parsing

--- remote/src/test/java/com/philheenan/remote/RemoteBaseTest.java
+++ remote/src/test/java/com/philheenan/remote/RemoteBaseTest.java
@@ -1,7 +1,18 @@
 package com.philheenan.remote;
 
-import junit.framework.TestCase;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.Before;
 
-public class RemoteBaseTest extends TestCase {
+public class RemoteBaseTest {
 
+  protected Gson gson;
+
+  @Before public void setUp() {
+    initialiseGson();
+  }
+
+  private void initialiseGson() {
+    gson = new GsonBuilder().create();
+  }
 }
