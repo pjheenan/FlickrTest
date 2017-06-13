1fe53a20f9d286c7ad3874cc6bee54675261a60b
Phil Heenan <pjheenan@gmail.com>
2017-06-13 08:54:10 +0100

    flesh out the remote gateway for public feed handling
    returns an observable to avoid blocking on potentially long requests. The response (as an api entity) is mapped to a domain model object and returned through the onNext. As a single request (no paging) the process will complete on returning a successful response.

--- remote/src/main/java/com/philheenan/remote/gateway/FeedRemoteGatewayImpl.java
+++ remote/src/main/java/com/philheenan/remote/gateway/FeedRemoteGatewayImpl.java
@@ -3,16 +3,46 @@ package com.philheenan.remote.gateway;
 import com.philheenan.domain.action.DomainRequest;
 import com.philheenan.domain.action.loadfeed.FeedRemoteGateway;
 import com.philheenan.domain.model.FeedPage;
+import com.philheenan.remote.client.FeedClient;
+import com.philheenan.remote.entity.FeedEntity;
+import com.philheenan.remote.request.FeedRequest;
+import com.philheenan.remote.request.PublicFeedsRequest;
+import retrofit2.Call;
 import rx.Observable;
 import rx.Subscriber;
 
 public class FeedRemoteGatewayImpl implements FeedRemoteGateway {
 
+  protected FeedClient feedClient;
+
   @Override public Observable<FeedPage> loadFeed(DomainRequest request) {
+    final FeedRequest remoteRequest = buildFeedRequest();
     return Observable.create(new Observable.OnSubscribe<FeedPage>() {
       @Override public void call(Subscriber<? super FeedPage> subscriber) {
-
+        try {
+          Call call = feedClient.getPublicFeed(remoteRequest.queryParams);
+          processResponse(subscriber, (FeedEntity) call.execute().body());
+        } catch (Exception e) {
+          processError(subscriber, e);
+        }
       }
     });
+  }
+
+  protected void processResponse(Subscriber<? super FeedPage> subscriber, FeedEntity feedEntity) {
+    subscriber.onNext(mapResponse(feedEntity));
+    subscriber.onCompleted();
+  }
+
+  protected void processError(Subscriber<? super FeedPage> subscriber, Throwable error) {
+    subscriber.onError(error);
+  }
+
+  private FeedRequest buildFeedRequest() {
+    return new PublicFeedsRequest();
+  }
+
+  private FeedPage mapResponse(FeedEntity entity) {
+    return new FeedPage();
   }
 }
--- remote/src/test/java/com/philheenan/remote/gateway/FeedRemoteGatewayImplTest.java
+++ remote/src/test/java/com/philheenan/remote/gateway/FeedRemoteGatewayImplTest.java
@@ -2,20 +2,66 @@ package com.philheenan.remote.gateway;
 
 import com.philheenan.domain.action.DomainRequest;
 import com.philheenan.domain.action.loadfeed.FeedRemoteGateway;
+import com.philheenan.domain.model.FeedPage;
 import com.philheenan.remote.RemoteBaseTest;
-import org.junit.Assert;
+import com.philheenan.remote.entity.FeedEntity;
 import org.junit.Before;
 import org.junit.Test;
+import rx.Observer;
+import rx.Subscriber;
+import rx.observers.TestSubscriber;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class FeedRemoteGatewayImplTest extends RemoteBaseTest {
 
   private FeedRemoteGateway gateway = new FeedRemoteGatewayImpl();
+  private FeedPage feedPageResult = null;
+  private boolean onCompletedCalled = false;
+  private boolean onErrorCalled = false;
 
   @Before public void setUp() {
     super.setUp();
   }
 
   @Test public void test_WhenLoadFeedCalled_thenObservableReturned() {
-    Assert.assertNotNull(gateway.loadFeed(new DomainRequest()));
+    assertNotNull(gateway.loadFeed(new DomainRequest()));
+  }
+
+  @Test public void test_whenResponseReturned_thenSubscriberCalled() {
+    Subscriber<FeedPage> subscriber = new TestSubscriber<>(arrangeTestSubscriberDelegate());
+    ((FeedRemoteGatewayImpl) gateway).processResponse(subscriber, new FeedEntity());
+
+    assertNotNull(feedPageResult);
+    assertTrue(onCompletedCalled);
+    assertFalse(onErrorCalled);
+  }
+
+  @Test public void test_whenErrorReturned_thenSubscriberErrorCalled() {
+    Subscriber<FeedPage> subscriber = new TestSubscriber<>(arrangeTestSubscriberDelegate());
+    ((FeedRemoteGatewayImpl) gateway).processError(subscriber, new NullPointerException());
+
+    assertNull(feedPageResult);
+    assertFalse(onCompletedCalled);
+    assertTrue(onErrorCalled);
+  }
+
+  private Observer<FeedPage> arrangeTestSubscriberDelegate() {
+    return new Observer<FeedPage>() {
+      @Override public void onCompleted() {
+        onCompletedCalled = true;
+      }
+
+      @Override public void onError(Throwable e) {
+        onErrorCalled = true;
+      }
+
+      @Override public void onNext(FeedPage feedPage) {
+        feedPageResult = feedPage;
+      }
+    };
   }
 }
