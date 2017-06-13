635af2aeb8ccd55a7ae5fce7d2d6e9fd8461b663
Phil Heenan <pjheenan@gmail.com>
2017-06-13 11:21:45 +0100

    make data available for view to load
    when `viewstate` set to loaded then the data should be available for the view to load

--- presentation/src/main/java/com/philheenan/presentation/publicfeed/presenter/PublicFeedPresenter.java
+++ presentation/src/main/java/com/philheenan/presentation/publicfeed/presenter/PublicFeedPresenter.java
@@ -8,12 +8,14 @@ import com.philheenan.domain.model.ImageItem;
 import com.philheenan.presentation.publicfeed.Presenter;
 import com.philheenan.presentation.publicfeed.viewmodel.PublicFeedViewModel;
 import com.philheenan.presentation.publicfeed.viewmodel.ViewStates;
+import java.util.List;
 
 public class PublicFeedPresenter implements Presenter<PublicFeedViewModel>, LoadFeedOutput {
 
   PublicFeedViewModel viewModel;
   LoadFeedInteractor interactor;
   InteractorInvoker invoker;
+  FeedPage data;
 
   @Override public void setViewModel(PublicFeedViewModel viewModel) {
     this.viewModel = viewModel;
@@ -25,18 +27,24 @@ public class PublicFeedPresenter implements Presenter<PublicFeedViewModel>, Load
   }
 
   @Override public void finish() {
+
   }
 
   @Override public void onFeedLoaded(FeedPage imageFeed) {
     if (imageFeed.imageItems == null || imageFeed.imageItems.isEmpty()) {
       viewModel.setViewState(ViewStates.EMPTY);
     } else {
+      data = imageFeed;
       viewModel.setViewState(ViewStates.LOADED);
     }
   }
 
   @Override public void onFeedError(Error error) {
     viewModel.setViewState(ViewStates.ERROR);
+  }
+
+  public List<ImageItem> loadImages() {
+    return data.imageItems;
   }
 
   public void onItemSelected(ImageItem item) {
--- presentation/src/test/java/com/philheenan/presentation/publicfeed/presenter/PublicFeedPresenterTest.java
+++ presentation/src/test/java/com/philheenan/presentation/publicfeed/presenter/PublicFeedPresenterTest.java
@@ -8,10 +8,12 @@ import com.philheenan.domain.model.ImageItem;
 import com.philheenan.presentation.publicfeed.viewmodel.PublicFeedViewModel;
 import com.philheenan.presentation.publicfeed.viewmodel.ViewStates;
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
@@ -21,16 +23,14 @@ public class PublicFeedPresenterTest {
 
   @Before public void setUp() {
     presenter = new PublicFeedPresenter();
+    arrangePresenter();
   }
 
   @Test public void test_whenViewModelSet_thenViewModelAvailable() {
-    arrangePresenter();
     Assert.assertNotNull(presenter.viewModel);
   }
 
   @Test public void test_whenStartCalled_thenInteractorCalled() {
-    arrangePresenter();
-
     presenter.start();
 
     verify(presenter.interactor).setOutput(any(LoadFeedOutput.class));
@@ -38,7 +38,6 @@ public class PublicFeedPresenterTest {
   }
 
   @Test public void test_whenSelectedItemCalled_thenViewModelCalled() {
-    arrangePresenter();
     ImageItem item = new ImageItem();
     presenter.onItemSelected(item);
 
@@ -46,34 +45,42 @@ public class PublicFeedPresenterTest {
   }
 
   @Test public void test_whenStartCalled_thenViewStateLoading() {
-    arrangePresenter();
     presenter.start();
 
     verify(presenter.viewModel).setViewState(eq(ViewStates.LOADING));
   }
 
   @Test public void test_whenErrorReturned_thenViewStateError() {
-    arrangePresenter();
     presenter.onFeedError(new Error());
 
     verify(presenter.viewModel).setViewState(eq(ViewStates.ERROR));
   }
 
   @Test public void test_whenEmptyResultCalled_thenViewStateEmpty() {
-    arrangePresenter();
     presenter.onFeedLoaded(new FeedPage());
 
     verify(presenter.viewModel).setViewState(eq(ViewStates.EMPTY));
   }
 
   @Test public void test_whenPopulatedResultCalled_thenViewStateLoaded() {
-    arrangePresenter();
-    FeedPage page = new FeedPage();
-    page.imageItems = new ArrayList<>();
-    page.imageItems.add(new ImageItem());
+    FeedPage page = arrangeFeedPage();
     presenter.onFeedLoaded(page);
 
     verify(presenter.viewModel).setViewState(eq(ViewStates.LOADED));
+  }
+
+  @Test public void test_whenDataReturnedAndLoadImagesCalled_thenResponseDataIsReturned() {
+    FeedPage page = arrangeFeedPage();
+    presenter.onFeedLoaded(page);
+    List<ImageItem> output = presenter.loadImages();
+    assertEquals(page.imageItems, output);
+  }
+
+  private FeedPage arrangeFeedPage() {
+    FeedPage page = new FeedPage();
+    page.imageItems = new ArrayList<>();
+    page.imageItems.add(new ImageItem());
+    return page;
   }
 
   private void arrangePresenter() {
