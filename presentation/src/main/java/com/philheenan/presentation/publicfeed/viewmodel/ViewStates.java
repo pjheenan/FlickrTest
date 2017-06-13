1bc98e9cca849ee5904a1aabfeab5071e8eb6d9f
Phil Heenan <pjheenan@gmail.com>
2017-06-13 10:52:07 +0100

    define potential view states
    a consistent mechanism to manage view decoration on how the view should be updated

---
+++ presentation/src/main/java/com/philheenan/presentation/publicfeed/viewmodel/ViewStates.java
@@ -0,0 +1,12 @@
+package com.philheenan.presentation.publicfeed.viewmodel;
+
+public class ViewStates {
+
+  public static final int LOADING = 0;
+  public static final int LOADED = 1;
+  public static final int EMPTY = 2;
+  public static final int ERROR = 3;
+
+  private ViewStates() {
+  }
+}
